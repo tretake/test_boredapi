47906962e10541af3e14521288d537d6cf7f99cc
ruan <ruanlourencosilva.rj@gmail.com>
2024-05-17 21:10:23 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+string apiUrl = "https://www.boredapi.com/api/activity/";
+
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+
+        HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+
+        if (response.IsSuccessStatusCode)
+        {
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            using (JsonDocument doc = JsonDocument.Parse(responseBody))
+            {
+
+                JsonElement root = doc.RootElement;
+
+                if (root.TryGetProperty("activity", out JsonElement ElementoAtividade))
+                {
+                    string atividade = ElementoAtividade.ToString();
+
+                    Console.WriteLine("Response from API:");
+                    Console.WriteLine(atividade);
+
+                }
+
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Failed to fetch data from the API. Status code: {response.StatusCode}");
+        }
+    }
+    catch (HttpRequestException ex)
+    {
+        Console.WriteLine($"An error occurred while making the request: {ex.Message}");
+    }
+}
